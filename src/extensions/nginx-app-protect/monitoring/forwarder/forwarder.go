cc1ae99b6b79a255d560a2574be81d011f4aebd5
m.gougam <m.gougam@f5.com>
2022-09-06 20:39:51 +0100

    feat: nap monitoring;

---
+++ src/extensions/nginx-app-protect/monitoring/forwarder/forwarder.go
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) F5 Inc. 2022
+ * All rights reserved.
+ *
+ * No part of the software may be reproduced or transmitted in any
+ * form or by any means, electronic or mechanical, for any purpose,
+ * without express written permission of F5 Inc.
+ */
+
+package forwarder
+
+import (
+	"context"
+	"sync"
+
+	"github.com/sirupsen/logrus"
+
+	pb "github.com/nginx/agent/sdk/v2/proto"
+	models "github.com/nginx/agent/sdk/v2/proto/events"
+)
+
+const (
+	componentName = "forwarder"
+)
+
+var (
+	// logging fields for the component
+	componentLogFields = logrus.Fields{
+		"component": componentName,
+	}
+)
+
+// Forwarder is the interface implemented by forwarders who want to
+// forward Security Event to a broker on a given subject.
+type Forwarder interface {
+	// Forward messages received on processed chan to the broker on the specified subject
+	Forward(ctx context.Context, wg *sync.WaitGroup, processed <-chan *models.Event)
+}
+
+// Broker is the interface implemented by a Message Queue to publish
+// messages on a specified subject.
+type Broker interface {
+	// Publish will publish to the broker and wait for an ACK
+	Publish(subject string, data []byte) error
+}
+
+// Client for Forwarder with capability of logging.
+type Client struct {
+	logger  *logrus.Entry
+	channel pb.Ingester_StreamEventReportClient
+}
+
+// NewClient gives you a Client for forwarding.
+func NewClient(channel pb.Ingester_StreamEventReportClient) (*Client, error) {
+	var c Client
+
+	c.logger = logrus.WithFields(componentLogFields)
+	c.channel = channel
+	return &c, nil
+}
+
+// Forward messages received on processed chan to broker on specified subject.
+func (c *Client) Forward(ctx context.Context, wg *sync.WaitGroup, processed <-chan *models.Event) {
+	defer wg.Done()
+
+	c.logger.Debugf("Forwarding security events to events service")
+
+	for {
+		select {
+		case event := <-processed:
+			c.logger.Debugf("Forwarding: %v", event)
+
+			err := c.channel.Send(&models.EventReport{Events: []*models.Event{event}})
+			if err != nil {
+				c.logger.Errorf("Error while publishing event to the gRPC events service: %v, event: %v", err, event)
+			}
+
+		case <-ctx.Done():
+			_, err := c.channel.CloseAndRecv()
+			if err != nil {
+				c.logger.Errorf("Error while closing events service gRPC stream: %v", err)
+			}
+			c.logger.Info("Context cancellation, forwarder is wrapping up...")
+			return
+		}
+	}
+}
---
+++ src/extensions/nginx-app-protect/monitoring/processor/processor_test.go
@@ -0,0 +1,120 @@
+/*
+ * Copyright (C) F5 Inc. 2022
+ * All rights reserved.
+ *
+ * No part of the software may be reproduced or transmitted in any
+ * form or by any means, electronic or mechanical, for any purpose,
+ * without express written permission of F5 Inc.
+ */
+
+package processor
+
+import (
+	"context"
+	"io/ioutil"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/sirupsen/logrus"
+
+	pb "github.com/nginx/agent/sdk/v2/proto/events"
+	nap_monitoring "github.com/nginx/agent/v2/src/extensions/nginx-app-protect/monitoring"
+)
+
+const (
+	// in seconds
+	eventWaitTimeout = 5
+
+	numWorkers = 4
+
+	mockSigDBFile   = "./testdata/mock-sigs"
+	nonexistentFile = "NonexistentFile"
+)
+
+func TestNAPWAFProcess(t *testing.T) {
+	testCases := []struct {
+		testName   string
+		testFile   string
+		expected   *pb.Event
+		isNegative bool
+		fileExists bool
+	}{
+		{
+			testName: "PassedEvent",
+			testFile: "./testdata/expanded_nap_waf.log.txt",
+			expected: nil,
+			// Event will not be generated because violationRating = 0, so it will be ignored as a low-risk event.
+			isNegative: false,
+			fileExists: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.testName, func(t *testing.T) {
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			sigDBFile := mockSigDBFile
+			if !tc.fileExists {
+				sigDBFile = nonexistentFile
+			}
+
+			collect := make(chan *nap_monitoring.RawLog, 2)
+
+			processed := make(chan *pb.Event, 2)
+
+			log := logrus.New()
+			log.SetLevel(logrus.DebugLevel)
+
+			p, err := GetClient(&Config{
+				Logger:                log.WithField("extension", "test"),
+				Workers:               numWorkers,
+				SigDBFile:             sigDBFile,
+				SigDBFilePollInterval: 1,
+			})
+			if err != nil {
+				t.Fatalf("Could not get a Processor Client: %s", err)
+			}
+
+			// This is to simulate the behavior of the necessary DB file
+			// not existing prior to getting the processor client, but knowing
+			// that later at some point the DB file will exist and we'll
+			// populate the sigIdToSigName map with it in the background.
+			if !tc.fileExists {
+				p.sigDBFile = mockSigDBFile
+			}
+
+			// make sure the tests actually wait on wg and
+			// verify that they are closed on cancel()
+			wg := &sync.WaitGroup{}
+
+			wg.Add(1)
+			// Start Processor
+			go p.Process(ctx, wg, collect, processed)
+
+			// Briefly sleep so map can be reconciled before event is collected
+			// and processed
+			if !tc.fileExists {
+				time.Sleep(2 * time.Second)
+			}
+
+			input, err := ioutil.ReadFile(tc.testFile)
+			if err != nil {
+				t.Fatalf("Error while reading the logfile %s: %v", tc.testFile, err)
+			}
+
+			collect <- &nap_monitoring.RawLog{Origin: nap_monitoring.NAPWAF, Logline: string(input)}
+
+			select {
+			case event := <-processed:
+				t.Logf("Got event: %v", event)
+			case <-time.After(eventWaitTimeout * time.Second):
+				// for negative test, there should not be an event generated.
+				if !tc.isNegative {
+					t.Error("Should receive security violation event, and should not be timeout.")
+				}
+			}
+		})
+	}
+}
