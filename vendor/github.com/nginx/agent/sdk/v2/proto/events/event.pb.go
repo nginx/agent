7675f507a0794da65df383d58a14a836e8d3d4ae
dhurley <djhurley1990@gmail.com>
2022-09-26 12:04:20 +0100

    Updated DataplaneStatus to include new field AgentActivityStatus

--- vendor/github.com/nginx/agent/sdk/v2/proto/events/event.pb.go
+++ vendor/github.com/nginx/agent/sdk/v2/proto/events/event.pb.go
@@ -123,7 +123,6 @@ func (m *Metadata) GetCategory() string {
 type Event struct {
 	Metadata *Metadata `protobuf:"bytes,1,opt,name=Metadata,proto3" json:"metadata"`
 	// Types that are valid to be assigned to Data:
-	//
 	//	*Event_ActivityEvent
 	Data                 isEvent_Data `protobuf_oneof:"data"`
 	XXX_NoUnkeyedLiteral struct{}     `json:"-"`
