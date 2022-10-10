4ca4e3efba7242ddf7e43e3878368fd63b39deb0
Aphral Griffin <a.griffin@f5.com>
2022-10-10 18:02:54 +0100

    fix failing performance tests

--- sdk/config_helpers.go
+++ sdk/config_helpers.go
@@ -87,7 +87,6 @@ func GetNginxConfig(
 	systemId string,
 	allowedDirectories map[string]struct{},
 ) (*proto.NginxConfig, error) {
-	log.Infof("Allowed Directories: %v",allowedDirectories)
 	payload, err := crossplane.Parse(confFile,
 		&crossplane.ParseOptions{
 			SingleFile:         false,
