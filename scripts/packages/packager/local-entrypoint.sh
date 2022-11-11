1ddba8518f907db74a8de22e4660311ccb1d310f
dhurley <djhurley1990@gmail.com>
2022-11-11 14:30:25 +0000

    Update swap.go to only log once on startup if swap metrics can't be collected

--- scripts/packages/packager/local-entrypoint.sh
+++ scripts/packages/packager/local-entrypoint.sh
@@ -19,7 +19,7 @@ cp build/nginx-agent /staging/usr/local/bin
 
 chmod +x /staging/usr/local/etc/rc.d/nginx-agent
 
-VERSION="$(git describe --match 'v[0-9]*' --abbrev=0 | tr -d 'v')-SNAPSHOT" envsubst < scripts/packages/manifest > /staging/+MANIFEST
+VERSION="$(git describe --match 'v[0-9]*' --abbrev=0 | tr -d 'v')-SNAPSHOT-$(git rev-parse --short HEAD)" envsubst < scripts/packages/manifest > /staging/+MANIFEST
 
 pkg -o ABI="FreeBSD:13:amd64" create --format txz  \
     -m /staging \
@@ -31,7 +31,7 @@ pkg -o ABI="FreeBSD:13:amd64" create --format txz  \
 # but since version 1.17.0 pkg will now always create a file with the extesion pkg no matter what the format is. 
 # See 1.17.0 release notes for more info: https://cgit.freebsd.org/ports/commit/?id=e497a16a286972bfcab908209b11ee6a13d99dc9
 cd build/
-ln -s nginx-agent-"$(git describe --match 'v[0-9]*' --abbrev=0 | tr -d 'v')"-SNAPSHOT.pkg nginx-agent-"$(git describe --match 'v[0-9]*' --abbrev=0 | tr -d 'v')"-SNAPSHOT.txz 
+ln -s nginx-agent-"$(git describe --match 'v[0-9]*' --abbrev=0 | tr -d 'v')"-SNAPSHOT-"$(git rev-parse --short HEAD)".pkg nginx-agent-"$(git describe --match 'v[0-9]*' --abbrev=0 | tr -d 'v')"-SNAPSHOT-"$(git rev-parse --short HEAD)".txz 
 cd ..
 
 rm -rf /staging
