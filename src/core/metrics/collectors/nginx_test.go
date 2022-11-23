8d62bb1e124ee366346f993f91d1c0e432f43c76
o.omahony <o.omahony@f5.com>
2022-11-23 18:01:17 +0000

    removed unnecessary changes

--- src/core/metrics/collectors/nginx_test.go
+++ src/core/metrics/collectors/nginx_test.go
@@ -184,7 +184,7 @@ func TestNginxCollector_UpdateConfig(t *testing.T) {
 			mockNginxSource2,
 		},
 		collectorConf: &metrics.NginxCollectorConfig{
-			NginxId: "124",
+			NginxId: "123",
 		},
 		env: env,
 	}
