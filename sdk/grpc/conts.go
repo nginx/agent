1ab9444c1d0f153814832cbeeef544bc555b775a
Donal Hurley <djhurley1990@gmail.com>
2022-11-30 13:53:32 +0000

    Added copyright header to source files (#122)

--- sdk/grpc/conts.go
+++ sdk/grpc/conts.go
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) F5, Inc.
+ *
+ * This source code is licensed under the Apache License, Version 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 package grpc
 
 const (
