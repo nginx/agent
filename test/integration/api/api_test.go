9e9a891e588212932d257cc8be53aa5893653b9f
Aphral Griffin <a.griffin@f5.com>
2022-12-07 10:38:06 +0000

    integration tests

--- test/integration/api/api_test.go
+++ test/integration/api/api_test.go
@@ -1,74 +1,82 @@
 package api
 
 import (
-    "context"
-    "fmt"
+	"fmt"
 	"net/http"
-    "testing"
-
-    "github.com/testcontainers/testcontainers-go"
-    "github.com/testcontainers/testcontainers-go/wait"
+	"strings"
+	"strconv"
+	"testing"
+	"github.com/go-resty/resty/v2"
+	"github.com/stretchr/testify/assert"
 )
 
-type nginxContainer struct {
-    testcontainers.Container
-    URI string
+func TestAPI (t *testing.T) {
+    port := 9091
+
+    client := resty.New()
+
+    url := fmt.Sprintf("http://localhost:%d/metrics", port)
+    
+
+    resp, err := client.R().EnableTrace().Get(url)
+
+    assert.NoError(t, err)
+    assert.Equal(t, http.StatusOK, resp.StatusCode())
+    assert.Contains(t, string(resp.String()), "system_cpu_system")
+
+    assert.NoError(t, err)
+
+	printResult(resp, err)
+
+	metrics := strings.Split(resp.String(), "\n")
+
+	i := 0
+
+	for _, metric := range metrics {
+		if metric[0:1] != "#" {
+			metrics[i] = metric
+			i++
+		}
+	}
+	metrics = metrics[:i]
+
+	for _, m := range metrics{
+		metric := strings.Split(m, " ")
+
+		switch {
+		case strings.Contains(metric[0], "system_cpu_system"):
+				value, _ := strconv.ParseFloat(metric[1], 64)
+				assert.Greater(t, value, float64(0))
+		
+		case strings.Contains(metric[0], "system_mem_used_all"):
+			value, _ := strconv.ParseFloat(metric[1], 64)
+			assert.Greater(t, value, float64(0))
+
+		case strings.Contains(metric[0], "container_cpu_cores"):
+			value, _ := strconv.ParseFloat(metric[1], 64)
+			assert.Greater(t, value, float64(0))
+
+		case strings.Contains(metric[0], "nginx_workers_count"):
+			value, _ := strconv.ParseFloat(metric[1], 64)
+			assert.Greater(t, value, float64(0))
+		}
+		
+	}
 }
 
-func setupNginx(ctx context.Context) (*nginxContainer, error) {
-    req := testcontainers.ContainerRequest{
-        Image:        "nginx",
-        ExposedPorts: []string{"80/tcp"},
-        WaitingFor:   wait.ForHTTP("/"),
-    }
-    container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
-        ContainerRequest: req,
-        Started:          true,
-    })
-    if err != nil {
-        return nil, err
-    }
 
-    ip, err := container.Host(ctx)
-    if err != nil {
-        return nil, err
-    }
 
-    mappedPort, err := container.MappedPort(ctx, "80")
-    if err != nil {
-        return nil, err
-    }
 
-    uri := fmt.Sprintf("http://%s:%s", ip, mappedPort.Port())
-
-    return &nginxContainer{Container: container, URI: uri}, nil
+func printResult(resp *resty.Response, err error) *resty.Response {
+	fmt.Println("Response Info:")
+	fmt.Println("  Error      :", err)
+	fmt.Println("  Status Code:", resp.StatusCode())
+	fmt.Println("  Status     :", resp.Status())
+	fmt.Println("  Proto      :", resp.Proto())
+	fmt.Println("  Time       :", resp.Time())
+	fmt.Println("  Received At:", resp.ReceivedAt())
+	fmt.Println("  Body       :\n", resp)
+	fmt.Println()
+	return resp
 }
 
-func TestIntegrationNginxLatestReturn(t *testing.T) {
-    if testing.Short() {
-        t.Skip("skipping integration test")
-    }
-
-    ctx := context.Background()
-
-    nginxC, err := setupNginx(ctx)
-    if err != nil {
-        t.Fatal(err)
-    }
-
-    // Clean up the container after the test is complete
-    t.Cleanup(func() {
-        if err := nginxC.Terminate(ctx); err != nil {
-            t.Fatalf("failed to terminate container: %s", err)
-        }
-    })
-
-    resp, err := http.Get(nginxC.URI)
-    if err != nil {
-        t.Fatal(err)
-    }
-
-    if resp.StatusCode != http.StatusOK {
-        t.Fatalf("Expected status code %d. Got %d.", http.StatusOK, resp.StatusCode)
-    }
-}
