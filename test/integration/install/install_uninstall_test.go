4da18c06400940aba3a17ffb919f6560e2679d38
Dean Coakley <d.coakley@f5.com>
2023-01-30 18:03:35 +0000

    Add base image override

--- test/integration/install/install_uninstall_test.go
+++ test/integration/install/install_uninstall_test.go
@@ -38,8 +38,10 @@ func setupTestContainer(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	t.Cleanup(cancel)
 
-	require.NoError(t, comp.WithEnv(
-		map[string]string{"PACKAGE_NAME": os.Getenv("PACKAGE_NAME")},
+	require.NoError(t, comp.WithEnv(map[string]string{
+		"PACKAGE_NAME": os.Getenv("PACKAGE_NAME"),
+		"BASE_IMAGE":   os.Getenv("BASE_IMAGE"),
+	},
 	).Up(ctx, compose.Wait(true)), "compose.Up()")
 
 	agentContainer, err = comp.ServiceContainer(context.TODO(), "agent")
