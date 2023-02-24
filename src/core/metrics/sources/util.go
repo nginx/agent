c9d2adf0606ab703729380156f29781fd478cf45
dhurley <djhurley1990@gmail.com>
2023-02-24 15:11:49 +0000

    Stop repeated warnings in container cpu metric source

---
+++ src/core/metrics/sources/util.go
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) F5, Inc.
+ *
+ * This source code is licensed under the Apache License, Version 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package sources
+
+import (
+	"sync"
+
+	log "github.com/sirupsen/logrus"
+)
+
+var once = &sync.Once{}
+
+func LogMetricError(message string) {
+	once.Do(func(){
+		log.Warnf(message)
+	})
+	log.Tracef(message)
+}
