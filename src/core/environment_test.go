6592a385c34107c1a97f4a9903cd75cd8a6dc163
Amardeep Chawla <a.chawla@f5.com>
2023-03-09 11:44:16 -0800

    add test with no name in release file

--- src/core/environment_test.go
+++ src/core/environment_test.go
@@ -55,6 +55,20 @@ VERSION_CODENAME=focal
 UBUNTU_CODENAME=focal
 `
 
+const osReleaseInfoWithNoName = `
+VERSION="20.04.5 LTS (Focal Fossa)"
+VERSION_ID="20.04"
+ID=ubuntu
+ID_LIKE=debian
+PRETTY_NAME="Ubuntu 20.04.5 LTS"
+HOME_URL="https://www.ubuntu.com"
+SUPPORT_URL=\"https://help.ubuntu.com/"
+BUG_REPORT_URL=\"https://bugs.launchpad.net/ubuntu/"
+PRIVACY_POLICY_URL="https://www.ubuntu.com/legal/terms-and-policies/privacy-policy"
+VERSION_CODENAME=focal
+UBUNTU_CODENAME=focal
+`
+
 var (
 	// set at buildtime
 	envMountInfo = [10]string{
@@ -555,6 +569,17 @@ func TestParseOsReleaseFile(t *testing.T) {
 				"VERSION":    "32 (Workstation Edition)",
 				"NAME":       "Fedora",
 				"ID":         "fedora",
+			},
+		},
+		{
+			name:             "os-release info with no name",
+			osReleaseContent: osReleaseInfoWithNoName,
+			expect: map[string]string{
+				"VERSION_ID":       "20.04",
+				"VERSION":          "20.04.5 LTS (Focal Fossa)",
+				"VERSION_CODENAME": "focal",
+				"NAME":             "unix",
+				"ID":               "ubuntu",
 			},
 		},
 	}
