1f1d81c9e741e7595055c0f0e57aba9babf9d2ec
Dean Coakley <d.coakley@f5.com>
2023-03-14 16:27:23 +0000

    Add lineCount to files in dirmap (#234)
    * Remove setting size in struct with no assert

--- sdk/config_helpers_test.go
+++ sdk/config_helpers_test.go
@@ -14,7 +14,6 @@ import (
 	"fmt"
 	"io"
 	"io/fs"
-	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -23,11 +22,12 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/gogo/protobuf/types"
 	"github.com/nginx/agent/sdk/v2/checksum"
 	SDKfiles "github.com/nginx/agent/sdk/v2/files"
 	"github.com/nginx/agent/sdk/v2/proto"
 	"github.com/nginx/agent/sdk/v2/zip"
+
+	"github.com/gogo/protobuf/types"
 	crossplane "github.com/nginxinc/nginx-go-crossplane"
 	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
@@ -170,15 +170,14 @@ var tests = []struct {
 							{
 								Name:        "nginx.conf",
 								Permissions: "0644",
-								Size_:       959,
+								Lines:       int32(51),
 							},
 							{
 								Name:        "ca.crt",
 								Permissions: "0644",
-								Size_:       959,
+								Lines:       int32(31),
 							},
 						},
-						Size_: 128,
 					},
 					{
 						Name:        "/tmp/testdata/root",
@@ -187,20 +186,16 @@ var tests = []struct {
 							{
 								Name:        "log-default.json",
 								Permissions: "0644",
-								Size_:       959,
 							},
 							{
 								Name:        "my-nap-policy.json",
 								Permissions: "0644",
-								Size_:       959,
 							},
 							{
 								Name:        "test.html",
 								Permissions: "0644",
-								Size_:       959,
 							},
 						},
-						Size_: 128,
 					},
 				},
 			},
@@ -234,7 +229,6 @@ var tests = []struct {
 							State:              []string{"Cork"},
 							OrganizationalUnit: nil,
 						},
-						Size_:                  1926,
 						Mtime:                  &types.Timestamp{Seconds: 1633343804, Nanos: 15240107},
 						SubjAltNames:           nil,
 						PublicKeyAlgorithm:     "RSA",
@@ -321,10 +315,8 @@ var tests = []struct {
 							{
 								Name:        "test.html",
 								Permissions: "0644",
-								Size_:       959,
 							},
 						},
-						Size_: 128,
 					},
 					{
 						Name:        "/tmp/testdata/nginx",
@@ -333,15 +325,14 @@ var tests = []struct {
 							{
 								Name:        "nginx2.conf",
 								Permissions: "0644",
-								Size_:       959,
+								Lines:       int32(41),
 							},
 							{
 								Name:        "ca.crt",
 								Permissions: "0644",
-								Size_:       959,
+								Lines:       int32(31),
 							},
 						},
-						Size_: 128,
 					},
 				},
 			},
@@ -375,7 +366,6 @@ var tests = []struct {
 							State:              []string{"Cork"},
 							OrganizationalUnit: nil,
 						},
-						Size_:                  1926,
 						Mtime:                  &types.Timestamp{Seconds: 1633343804, Nanos: 15240107},
 						SubjAltNames:           nil,
 						PublicKeyAlgorithm:     "RSA",
@@ -483,10 +473,9 @@ var tests = []struct {
 							{
 								Name:        "hello.conf",
 								Permissions: "0644",
-								Size_:       1672,
+								Lines:       int32(62),
 							},
 						},
-						Size_: 160,
 					},
 					{
 						Name:        "/tmp/testdata/nginx/other",
@@ -505,7 +494,6 @@ var tests = []struct {
 								Permissions: "0644",
 							},
 						},
-						Size_: 160,
 					},
 				},
 			},
@@ -626,6 +614,7 @@ func TestGetNginxConfig(t *testing.T) {
 				resultFile := resultDir.Files[fileIndex]
 				assert.Equal(t, expectedFile.Name, resultFile.Name)
 				assert.Equal(t, expectedFile.Permissions, resultFile.Permissions)
+				assert.Equal(t, expectedFile.Lines, resultFile.Lines, "unexpected line count for "+expectedFile.Name)
 			}
 		}
 
@@ -715,12 +704,12 @@ func TestGetStatusApiInfo(t *testing.T) {
 			require.Nil(t, err)
 
 			// Replace ip & ports in config with mock server ip & port
-			input, err := ioutil.ReadFile(test.fileName)
+			input, err := os.ReadFile(test.fileName)
 			assert.Nil(t, err)
 			splitUrl := strings.Split(server.URL, "//")[1]
 
 			output := bytes.Replace(input, []byte("127.0.0.1:80"), []byte(splitUrl), -1)
-			assert.NoError(t, ioutil.WriteFile(test.fileName, output, 0666))
+			assert.NoError(t, os.WriteFile(test.fileName, output, 0664))
 
 			result, err := GetStatusApiInfo(test.fileName)
 
@@ -912,7 +901,7 @@ server {
 		`,
 		},
 	} {
-		f, err := ioutil.TempFile(tmpDir, "conf")
+		f, err := os.CreateTemp(tmpDir, "conf")
 		assert.NoError(t, err)
 
 		_, err = f.Write([]byte(fmt.Sprintf("http{ %s }", tt.conf)))
@@ -985,7 +974,7 @@ func setUpDirectories() error {
 	}
 
 	for _, file := range files {
-		err := ioutil.WriteFile(file, []byte{}, 0644)
+		err := os.WriteFile(file, []byte{}, 0644)
 		if err != nil {
 			return err
 		}
@@ -1016,7 +1005,7 @@ func setUpFile(file string, content []byte) error {
 	if err != nil {
 		return err
 	}
-	err = ioutil.WriteFile(file, content, 0644)
+	err = os.WriteFile(file, content, 0644)
 	if err != nil {
 		return err
 	}
@@ -1112,7 +1101,7 @@ root foo/bar;`,
 		t.Run(tt.name, func(t *testing.T) {
 			tmpDir := t.TempDir()
 
-			f, err := ioutil.TempFile(tmpDir, "conf")
+			f, err := os.CreateTemp(tmpDir, "conf")
 			require.NoError(t, err)
 
 			_, err = f.WriteString(tt.input)
@@ -1168,10 +1157,8 @@ func TestUpdateNginxConfigFileWithAuxFile(t *testing.T) {
 								{
 									Name:        "app_protect_metadata.json",
 									Permissions: "0644",
-									Size_:       959,
 								},
 							},
-							Size_: 128,
 						},
 					},
 				},
@@ -1256,6 +1243,7 @@ func TestUpdateNginxConfigFileWithAuxFile(t *testing.T) {
 				resultFile := resultDir.Files[fileIndex]
 				assert.Equal(t, expectedFile.Name, resultFile.Name)
 				assert.Equal(t, expectedFile.Permissions, resultFile.Permissions)
+				assert.Equal(t, expectedFile.Lines, resultFile.Lines, "unexpected line count for "+expectedFile.Name)
 			}
 		}
 	}
@@ -1283,10 +1271,8 @@ func TestAddAuxfileToNginxConfig(t *testing.T) {
 								{
 									Name:        "app_protect_metadata.json",
 									Permissions: "0644",
-									Size_:       959,
 								},
 							},
-							Size_: 128,
 						},
 					},
 				},
@@ -1356,6 +1342,7 @@ func TestAddAuxfileToNginxConfig(t *testing.T) {
 				resultFile := resultDir.Files[fileIndex]
 				assert.Equal(t, expectedFile.Name, resultFile.Name)
 				assert.Equal(t, expectedFile.Permissions, resultFile.Permissions)
+				assert.Equal(t, expectedFile.Lines, resultFile.Lines, "unexpected line count for "+expectedFile.Name)
 			}
 		}
 	}
