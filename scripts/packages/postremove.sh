60d2e50a3c3d9d3ddcf7fb74bd16dd28aa2b33cf
Aphral Griffin <a.griffin@f5.com>
2023-04-18 17:28:59 +0100

    PR feedback

--- scripts/packages/postremove.sh
+++ scripts/packages/postremove.sh
@@ -7,9 +7,9 @@ if [ "$ID" = "freebsd" ]; then
     echo "Stop and remove nginx-agent service"
     service nginx-agent onestop >/dev/null 2>&1 || true
     sysrc -x nginx_agent_enable >/dev/null 2>&1 || true
-    echo "Removing etc directory"
+    echo "Removing /usr/local/etc/nginx-agent directory"
     rm -rf "/usr/local/etc/nginx-agent"
-    echo "Removing db directory"
+    echo "Removing /var/db/nginx-agent directory"
     rm -rf "/var/db/nginx-agent"
 elif command -V systemctl >/dev/null 2>&1; then
     echo "Stop and disable nginx-agent service"
@@ -17,13 +17,13 @@ elif command -V systemctl >/dev/null 2>&1; then
     systemctl disable nginx-agent >/dev/null 2>&1 || true
     echo "Running daemon-reload"
     systemctl daemon-reload || true
-    echo "Removing etc directory"
+    echo "Removing /etc/nginx-agent directory"
     rm -rf "/etc/nginx-agent"
-    echo "Removing lib directory"
+    echo "Removing /var/lib/nginx-agent directory"
     rm -rf "/var/lib/nginx-agent"
 fi
 
-echo "Removing run directory"
+echo "Removing /var/run/nginx-agent directory"
 rm -rf "/var/run/nginx-agent"
-echo "Removing log directory"
+echo "Removing /var/log/nginx-agent directory"
 rm -rf "/var/log/nginx-agent"
