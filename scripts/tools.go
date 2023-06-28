5777ec0dfceea7dba30d49fc7ff2d3909a57e298
Oliver O'Mahony <oliveromahony+github@gmail.com>
2023-06-28 16:52:14 +0100

    fixed tooling to pull from vendor and go.mod

--- scripts/tools.go
+++ scripts/tools.go
@@ -1,20 +1,22 @@
 //go:build tools
 // +build tools
 
+// https://www.jvt.me/posts/2022/06/15/go-tools-dependency-management/
+
 package tools
 
 import (
 	_ "github.com/alvaroloes/enumer"
+	_ "github.com/bufbuild/buf/cmd/buf"
+	_ "github.com/evilmartians/lefthook"
+	_ "github.com/go-swagger/go-swagger/cmd/swagger"
 	_ "github.com/gogo/protobuf/protoc-gen-gogo"
 	_ "github.com/gogo/protobuf/protoc-gen-gogofast"
 	_ "github.com/golang/mock/mockgen"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/goreleaser/nfpm/v2/cmd/nfpm"
 	_ "github.com/maxbrunsfeld/counterfeiter/v6"
 	_ "github.com/mwitkow/go-proto-validators/protoc-gen-govalidators"
-	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 	_ "github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc"
-	_ "github.com/go-swagger/go-swagger/cmd/swagger"
-	_ "github.com/bufbuild/buf/cmd/buf"
-	_ "github.com/goreleaser/nfpm/v2/cmd/nfpm"
-	_ "github.com/evilmartians/lefthook"
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 )
