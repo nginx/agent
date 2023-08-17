2e28994642ae6634a928f7854ecc674f0c2a8d17
Oliver O'Mahony <oliveromahony+github@gmail.com>
2023-08-17 14:04:52 +0100

    removed error monitoring

--- src/core/nginx.go
+++ src/core/nginx.go
@@ -307,7 +307,7 @@ func (n *NginxBinaryType) ValidateConfig(processId, bin, configLocation string, 
 }
 
 func (n *NginxBinaryType) validateConfigCheckResponse(response *bytes.Buffer, configLocation string) error {
-	if bytes.Contains(response.Bytes(), []byte("[emerg]")) || bytes.Contains(response.Bytes(), []byte("[alert]")) || bytes.Contains(response.Bytes(), []byte("[crit]")) || bytes.Contains(response.Bytes(), []byte("[error]")) {
+	if bytes.Contains(response.Bytes(), []byte("[emerg]")) || bytes.Contains(response.Bytes(), []byte("[alert]")) || bytes.Contains(response.Bytes(), []byte("[crit]")) {
 		return fmt.Errorf("error running nginx -t -c %v:\n%s", configLocation, response)
 	}
 
