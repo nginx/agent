266d858da474623e93f884cbd0f8ebd058a22bab
dhurley <djhurley1990@gmail.com>
2023-09-21 14:54:34 +0100

    Update tests

--- test/integration/grpc/grpc_config_apply_test.go
+++ test/integration/grpc/grpc_config_apply_test.go
@@ -1,6 +1,7 @@
 package grpc
 
 import (
+	"fmt"
 	"net"
 	"os"
 	"testing"
@@ -17,12 +18,55 @@ import (
 )
 
 const (
-	GRPC_ADDRESS  = ":54789"
 	GRPC_PROTOCOL = "tcp"
+	NGINX_CONF    = `
+#
+# /etc/nginx-agent/nginx-agent.conf
+#
+# Configuration file for NGINX Agent.
+#
+# This file is to track agent configuration values that are meant to be statically set. There  
+# are additional agent configuration values that are set via the API and agent install script
+# which can be found in /var/lib/nginx-agent/agent-dynamic.conf. 
+
+server:
+    host: host.docker.internal
+    grpcPort: %d
+
+api:
+    host: 0.0.0.0
+    port: 8038
+
+tls:
+    enable: false
+    skip_verify: true
+
+log:
+    level: info
+    path: /var/log/nginx-agent/
+
+nginx:
+    exclude_logs: ""
+    socket: "unix:/var/run/nginx-agent/nginx.sock"
+    config_reload_monitoring_period: 30s
+
+dataplane:
+    status:
+        poll_interval: 30s
+        report_interval: 24h
+
+metrics:
+    bulk_size: 20
+    report_interval: 1m
+    collection_interval: 15s
+    mode: aggregated
+
+config_dirs: "/etc/nginx:/usr/local/etc/nginx:/usr/share/nginx/modules:/etc/nms:/usr/local/nginx"
+`
 )
 
 func TestRegistrationAndConfigApply(t *testing.T) {
-	grpcListener, grpcClose := createListener(GRPC_ADDRESS)
+	grpcListener, grpcClose := createListener()
 	defer grpcClose()
 
 	srvOptions := sdkGRPC.DefaultServerDialOptions
@@ -37,6 +81,14 @@ func TestRegistrationAndConfigApply(t *testing.T) {
 			log.Fatal("error starting server")
 		}
 	}()
+
+	nginxConfig := fmt.Sprintf(NGINX_CONF, grpcListener.Addr().(*net.TCPAddr).Port)
+	err := os.WriteFile("nginx-agent.conf", []byte(nginxConfig), os.ModePerm)
+	defer func() {
+		err := os.Remove("./nginx-agent.conf")
+		assert.NoError(t, err)
+	}()
+	assert.NoError(t, err)
 
 	testContainer := utils.SetupTestContainerWithAgent(
 		t,
@@ -150,8 +202,8 @@ messageLoop:
 	}
 }
 
-func createListener(address string) (listener net.Listener, close func() error) {
-	listen, err := net.Listen(GRPC_PROTOCOL, address)
+func createListener() (listener net.Listener, close func() error) {
+	listen, err := net.Listen(GRPC_PROTOCOL, ":0")
 	if err != nil {
 		panic(err)
 	}
