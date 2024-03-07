8a54fe375400d2cc7998e9b863606f2dcf524e0d
Aphral Griffin <a.griffin@f5.com>
2024-03-07 14:26:04 +0000

    pr feedback

--- internal/plugin/metrics_plugin_test.go
+++ internal/plugin/metrics_plugin_test.go
@@ -191,7 +191,7 @@ func TestMetrics_CallProduce(t *testing.T) {
 			producer.ProduceReturns(test.entries, test.expectedProduceError)
 			failedAttempts := metrics.callProduce(context.TODO(), &producer, 0)
 
-			assert.Len(t, len(messagePipe.GetMessages()), test.expectedNumMessage)
+			assert.Len(t, messagePipe.GetMessages(), test.expectedNumMessage)
 			assert.Equal(t, test.expectedFailedAttempts, failedAttempts)
 		})
 	}
