dc675c8bdefa7185bde7e170434918443f3fd3a7
aphralG <108004222+aphralG@users.noreply.github.com>
2024-03-08 09:51:20 +0000

    Update internal/datasource/config/config_writer.go
    Co-authored-by: Donal Hurley <djhurley1990@gmail.com>

--- internal/datasource/config/config_writer.go
+++ internal/datasource/config/config_writer.go
@@ -66,7 +66,7 @@ func (cw *ConfigWriter) Rollback(ctx context.Context, skippedFiles CacheContent,
 ) error {
 	slog.Debug("Rolling back NGINX config changes due to error")
 	if cw.fileCache.CacheContent() == nil {
-		return fmt.Errorf("error rolling back, no cache found")
+		return fmt.Errorf("error rolling back, no instance file cache found for instance %s", instanceID)
 	}
 	for key, value := range cw.fileCache.CacheContent() {
 		if _, ok := skippedFiles[key]; ok {
