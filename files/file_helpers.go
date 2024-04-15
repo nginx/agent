8cc04e411a4032d47e8cc103e11bb5d705568df5
dhurley <djhurley1990@gmail.com>
2024-04-15 15:51:17 +0100

    Add comments

--- files/file_helpers.go
+++ files/file_helpers.go
@@ -3,6 +3,7 @@
 // This source code is licensed under the Apache License, Version 2.0 license found in the
 // LICENSE file in the root directory of this source tree.
 
+// Package files implements utility routines for gathering information about files nad their contents.
 package files
 
 import (
@@ -15,10 +16,13 @@ import (
 	"github.com/nginx/agent/v3/internal/uuid"
 )
 
+// GetPermissions returns a file's permissions as a string.
 func GetPermissions(fileMode os.FileMode) string {
 	return fmt.Sprintf("%#o", fileMode.Perm())
 }
 
+// GenerateConfigVersion returns a unique config version for a set of files.
+// The config version is calculated by joining the file hashes together and generating a unique ID.
 func GenerateConfigVersion(files []*v1.File) string {
 	var hashes string
 
@@ -29,6 +33,7 @@ func GenerateConfigVersion(files []*v1.File) string {
 	return uuid.Generate("%s", hashes)
 }
 
+// GenerateFileHash returns the hash value of a file's contents.
 func GenerateFileHash(filePath string) (string, error) {
 	f, err := os.Open(filePath)
 	if err != nil {
