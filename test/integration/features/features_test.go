523e5c2cb7225bed2c4dee45b0b9661a44ed1db3
dhurley <djhurley1990@gmail.com>
2024-05-07 14:21:50 +0100

    Fix tests

--- test/integration/features/features_test.go
+++ test/integration/features/features_test.go
@@ -20,7 +20,7 @@ func TestFeatures_NginxCountingEnabled(t *testing.T) {
 		t,
 		"features-nginx-counting-enabled",
 		"./test_configs/nginx-agent-counting.conf:/etc/nginx-agent/nginx-agent.conf",
-		"Starting Agent API HTTP server with port from config and TLS disabled",
+		"MetricsThrottle waiting for report ready",
 	)
 	utils.TestAgentHasNoErrorLogs(t, testContainer)
 
