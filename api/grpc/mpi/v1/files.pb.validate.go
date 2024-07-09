ca49517b6f4ea16e420c488a4c7741763425b1e2
oliveromahony <oliveromahony+github@gmail.com>
2024-07-09 16:19:22 +0100

    Add correlation id to update file request (#741)
    * added the message meta to the UpdateFileRequest

--- api/grpc/mpi/v1/files.pb.validate.go
+++ api/grpc/mpi/v1/files.pb.validate.go
@@ -1589,6 +1589,35 @@ func (m *UpdateFileRequest) validate(all bool) error {
 		}
 	}
 
+	if all {
+		switch v := interface{}(m.GetMessageMeta()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, UpdateFileRequestValidationError{
+					field:  "MessageMeta",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, UpdateFileRequestValidationError{
+					field:  "MessageMeta",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetMessageMeta()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return UpdateFileRequestValidationError{
+				field:  "MessageMeta",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return UpdateFileRequestMultiError(errors)
 	}
