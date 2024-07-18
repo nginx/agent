81cccd75e1752098e8532b3fa1fb65ab4f33caa6
Donal Hurley <djhurley1990@gmail.com>
2024-07-18 09:46:16 +0100

    Update ConfigApplyRequest and ConfigUploadRequest protos (#745)

--- internal/file/file_plugin_test.go
+++ internal/file/file_plugin_test.go
@@ -219,7 +219,6 @@ func TestFilePlugin_Process_ConfigUploadRequestTopic(t *testing.T) {
 	message := &mpi.ManagementPlaneRequest{
 		Request: &mpi.ManagementPlaneRequest_ConfigUploadRequest{
 			ConfigUploadRequest: &mpi.ConfigUploadRequest{
-				InstanceId: "123",
 				Overview: &mpi.FileOverview{
 					Files: []*mpi.File{
 						{
@@ -278,7 +277,6 @@ func TestFilePlugin_Process_ConfigUploadRequestTopic_Failure(t *testing.T) {
 	message := &mpi.ManagementPlaneRequest{
 		Request: &mpi.ManagementPlaneRequest_ConfigUploadRequest{
 			ConfigUploadRequest: &mpi.ConfigUploadRequest{
-				InstanceId: protos.GetNginxOssInstance([]string{}).GetInstanceMeta().GetInstanceId(),
 				Overview: &mpi.FileOverview{
 					Files: []*mpi.File{
 						{
