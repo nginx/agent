a44026f096707908fa65b27564df6004858b3f30
Oliver O'Mahony <oliveromahony+github@gmail.com>
2024-08-06 15:06:56 +0100

    leak test

--- src/core/metrics/sources/cgroup/leak_test.go
+++ src/core/metrics/sources/cgroup/leak_test.go
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
- package cgroup
+package cgroup
 
 import (
 	"testing"
--- src/core/tailer/leak_test.go
+++ src/core/tailer/leak_test.go
@@ -5,15 +5,14 @@
  * LICENSE file in the root directory of this source tree.
  */
 
- package tailer
+package tailer
 
- import (
-	 "testing"
- 
-	 "go.uber.org/goleak"
- )
- 
- func TestMain(m *testing.M) {
-	 goleak.VerifyTestMain(m)
- }
- 
+import (
+	"testing"
+
+	"go.uber.org/goleak"
+)
+
+func TestMain(m *testing.M) {
+	goleak.VerifyTestMain(m)
+}
