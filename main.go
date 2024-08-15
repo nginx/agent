b27d7ef30bdbc07f4f749ee9b3119c9466dd140c
Oliver O'Mahony <oliveromahony+github@gmail.com>
2024-08-15 11:14:41 +0100

    remove defer in main

--- main.go
+++ main.go
@@ -33,12 +33,6 @@ var (
 func main() {
 	config.InitFlags(version, commit)
 
-	defer func() {
-		if err := recover(); err != nil {
-			log.Printf("Recovered: %v", err)
-		}
-	}()
-
 	config.RegisterRunner(func(cmd *cobra.Command, _ []string) {
 		config.InitConfigurationFiles()
 
