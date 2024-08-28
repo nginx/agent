37396062ed762f5691ec86c7b7f08feb4e20b20c
Aphral Griffin <a.griffin@f5.com>
2024-08-28 16:10:13 +0100

    make deps

--- test/integration/vendor/github.com/nginx/agent/v2/src/core/nginx.go
+++ test/integration/vendor/github.com/nginx/agent/v2/src/core/nginx.go
@@ -906,6 +906,8 @@ func (n *NginxBinaryType) parseModulePath(dir string) ([]string, error) {
 }
 
 func (n *NginxBinaryType) UpdateLogs(existingLogs map[string]string, newLogs map[string]string) bool {
+	logMutex.Lock()
+	defer logMutex.Unlock()
 	logUpdated := false
 
 	for logFile, logFormat := range newLogs {
