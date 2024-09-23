f21a7ee99106586d157fd741c9b5e3834724a94a
Oliver O'Mahony <oliveromahony+github@gmail.com>
2024-09-23 13:02:05 +0100

    agent log to standard out as well as a file

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -88,8 +88,11 @@ func getLogWriter(logFile string) io.Writer {
 
 			return os.Stderr
 		}
-
-		return logFileHandle
+    
+		// Use io.MultiWriter to log to both Stdout and the file
+    	multiWriter := io.MultiWriter(os.Stdout, logFileHandle)
+		
+		return multiWriter
 	}
 
 	return os.Stderr
