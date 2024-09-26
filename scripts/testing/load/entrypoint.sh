5afc05da437a14f830e03b9d2f68e5130a321a29
Aphral Griffin <a.griffin@f5.com>
2024-09-26 16:45:18 +0100

    check pipeline

--- scripts/testing/load/entrypoint.sh
+++ scripts/testing/load/entrypoint.sh
@@ -5,7 +5,9 @@ go test -v -timeout 30s -cpuprofile load_cpu.pprof
 
 # copy profile to build for use in generate-pgo-profile
 cp load_cpu.pprof /agent/build/load_cpu.pprof
+cp benchmarks.json /agent/build/benchmarks.json
 
-# cat logs for debugging
+# cat logs & results for debugging
 cat /var/log/nginx-agent/agent.log
 cat /var/log/nginx-agent/opentelemetry-collector-agent.log
+cat /agent/build/benchmarks.json
