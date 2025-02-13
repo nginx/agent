04db67273cde6c095ff96fa344317bfe8968bcbe
Sean Breen <s.breen@f5.com>
2025-02-13 10:40:53 +0000

    remove test

--- internal/watcher/process/process_operator_test.go
+++ internal/watcher/process/process_operator_test.go
@@ -6,22 +6,12 @@
 package process
 
 import (
-	"context"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestNewProcessOperator(t *testing.T) {
 	po := NewProcessOperator()
 	assert.NotNil(t, po)
-}
-
-func TestProcessOperator_Processes(t *testing.T) {
-	ctx := context.Background()
-	po := &ProcessOperator{}
-	got, err := po.Processes(ctx)
-	require.NoError(t, err)
-	assert.NotEmpty(t, got)
 }
