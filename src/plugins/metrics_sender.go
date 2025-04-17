9c9f8383712e94a0fd2447c99c6ba293482ed393
rajesh bejjanki <r.bejjanki@f5.com>
2025-04-17 10:55:35 +0530

    fix: fixing import issue

--- src/plugins/metrics_sender.go
+++ src/plugins/metrics_sender.go
@@ -9,6 +9,7 @@ package plugins
 
 import (
 	"context"
+	"github.com/nginx/agent/sdk/v2"
 	agent_config "github.com/nginx/agent/sdk/v2/agent/config"
 	"github.com/nginx/agent/sdk/v2/client"
 	"github.com/nginx/agent/sdk/v2/proto"
