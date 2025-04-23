d13a27c84657f0805dfa3afe80d1abc8b77bd0ad
Nutsa Bidzishvili <n.bidzishvili@f5.com>
2025-04-23 10:47:59 +0100

    Add unit test for stderr/stdout error log  (#1054)
    * add unit test to check expected error log output for stderr/stdout
    
    * Update order of tests and labels

--- test/model/config.go
+++ test/model/config.go
@@ -72,3 +72,47 @@ func GetConfigContextWithNames(
 		NAPSysLogServers: syslogServers,
 	}
 }
+
+func GetConfigContextWithoutErrorLog(
+	accessLogName,
+	combinedAccessLogName,
+	ltsvAccessLogName,
+	instanceID string,
+	syslogServers []string,
+) *model.NginxConfigContext {
+	return &model.NginxConfigContext{
+		StubStatus: &model.APIDetails{
+			URL:      "",
+			Listen:   "",
+			Location: "",
+		},
+		PlusAPI: &model.APIDetails{
+			URL:      "",
+			Listen:   "",
+			Location: "",
+		},
+		AccessLogs: []*model.AccessLog{
+			{
+				Name:        accessLogName,
+				Format:      "$remote_addr - $remote_user [$time_local]",
+				Readable:    true,
+				Permissions: "0600",
+			},
+			{
+				Name: combinedAccessLogName,
+				Format: "$remote_addr - $remote_user [$time_local] " +
+					"\"$request\" $status $body_bytes_sent \"$http_referer\" \"$http_user_agent\"",
+				Readable:    true,
+				Permissions: "0600",
+			},
+			{
+				Name:        ltsvAccessLogName,
+				Format:      "ltsv",
+				Readable:    true,
+				Permissions: "0600",
+			},
+		},
+		InstanceID:       instanceID,
+		NAPSysLogServers: syslogServers,
+	}
+}
