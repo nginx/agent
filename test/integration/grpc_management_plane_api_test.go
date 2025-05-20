bba98b8b75f5b318ddf75434f2e4eab9bf3a06c7
Donal Hurley <d.hurley@f5.com>
2025-05-20 10:54:42 +0100

    Update tests

--- test/integration/grpc_management_plane_api_test.go
+++ test/integration/grpc_management_plane_api_test.go
@@ -310,7 +310,7 @@ func TestGrpc_ConfigApply(t *testing.T) {
 
 		performConfigApply(t, nginxInstanceID)
 
-		responses = getManagementPlaneResponses(t, 2)
+		responses = getManagementPlaneResponses(t, 1)
 		t.Logf("Config apply responses: %v", responses)
 
 		sort.Slice(responses, func(i, j int) bool {
@@ -319,8 +319,6 @@ func TestGrpc_ConfigApply(t *testing.T) {
 
 		assert.Equal(t, mpi.CommandResponse_COMMAND_STATUS_OK, responses[0].GetCommandResponse().GetStatus())
 		assert.Equal(t, "Config apply successful", responses[0].GetCommandResponse().GetMessage())
-		assert.Equal(t, mpi.CommandResponse_COMMAND_STATUS_OK, responses[1].GetCommandResponse().GetStatus())
-		assert.Equal(t, "Successfully updated all files", responses[1].GetCommandResponse().GetMessage())
 	})
 
 	t.Run("Test 3: Invalid config", func(t *testing.T) {
