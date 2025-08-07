633a3e329f4065bbdc727cd1308a2f72c9cb32f6
Sean Breen <s.breen@f5.com>
2025-08-07 10:29:40 +0100

    start changes

---
+++ test/docker/load/entrypoint.sh
@@ -0,0 +1,13 @@
+!/bin/bash
+
+# run load tests
+go test -v -timeout 30s -cpuprofile load_cpu.pprof
+
+# copy profile to build for use in generate-pgo-profile
+cp load_cpu.pprof /agent/build/load_cpu.pprof
+cp benchmarks.json /agent/build/benchmarks.json
+
+# cat logs & results for debugging
+cat /var/log/nginx-agent/agent.log
+cat /var/log/nginx-agent/opentelemetry-collector-agent.log
+cat /agent/build/benchmarks.json
