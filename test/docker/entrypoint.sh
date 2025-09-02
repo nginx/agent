9f444d5cd6dd89ffb5fdcef17a7a412c488df156
Sean Breen <s.breen@f5.com>
2025-09-02 14:25:50 +0100

    revert changes

--- test/docker/entrypoint.sh
+++ test/docker/entrypoint.sh
@@ -44,24 +44,22 @@ done
 cat /etc/nginx-agent/nginx-agent.conf;
 
 # start nginx-agent, pass args
-#echo "starting nginx-agent ..."
-#nginx-agent "$@" &
+echo "starting nginx-agent ..."
+nginx-agent "$@" &
 
-#agent_pid=$!
-#
-#if [ $? != 0 ]; then
-#    echo "couldn't start the agent, please check the log file"
-#    exit 1
-#fi
+agent_pid=$!
 
-echo "Not starting Nginx Agent as part of this test container"
+if [ $? != 0 ]; then
+    echo "couldn't start the agent, please check the log file"
+    exit 1
+fi
 
 wait_term()
 {
-#    wait ${agent_pid}
+    wait ${agent_pid}
     wait ${nginx_pid}
 }
 
 wait_term
 
-#echo "nginx-agent process has stopped, exiting."
+echo "nginx-agent process has stopped, exiting."
