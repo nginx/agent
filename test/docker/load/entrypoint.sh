afcac2ededef9255f1f13c680e0976739a43eed5
Sean Breen <s.breen@f5.com>
2025-09-12 11:25:35 +0100

    use correct paths, simplify extraction

--- test/docker/load/entrypoint.sh
+++ test/docker/load/entrypoint.sh
@@ -7,9 +7,8 @@ load_test() {
   echo "Running load tests..."
   pushd test/load
   go test -v -timeout 1m ./...
-
   cp benchmarks.json ${BENCHMARKS_DIR}
-  cp -r results ${BENCHMARKS_DIR}/results
+  cp -r results ${BENCHMARKS_DIR}
   popd
 }
 
@@ -17,28 +16,20 @@ load_test_with_profile() {
   echo "Running load tests with CPU Profiling enabled..."
   pushd test/load
   go test -v -timeout 1m ./... \
-    -cpuprofile ${BENCHMARKS_DIR}/metrics_load_cpu.pprof
-
-  cp benchmarks.json ${BENCHMARKS_DIR}/benchmarks.json
-  cp -r results ${BENCHMARKS_DIR}/results
-
+    -cpuprofile metrics_load_cpu.pprof
+  cp benchmarks.json ${BENCHMARKS_DIR}
+  cp -r results ${BENCHMARKS_DIR}
+  cp *.pprof ${BENCHMARKS_DIR}
   popd
 }
 
 ## Main script execution starts here
 mkdir -p ${BENCHMARKS_DIR}
-
 echo "Running in $(pwd)"
-# Run load tests
-#   $PROFILE - if true, run with CPU profiling enabled
 if [[ "$PROFILE" == "true" ]]; then
   echo "CPU Profiling is enabled."
-  load_test_with_profile || { echo "Load tests with profiling failed"; exit 1; }
+  load_test_with_profile || { echo "Load tests with cpu profiling failed"; exit 1; }
 else
   load_test || { echo "Load tests failed"; exit 1; }
 fi
-
-echo "Listing contents of ${BENCHMARKS_DIR}:"
-ls -la ${BENCHMARKS_DIR}
-
 echo "Done."
