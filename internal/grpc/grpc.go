ea48790ab3b7aeceaf855772b24b21161e3a4939
Akshay2191 <akshay2191@gmail.com>
2026-04-27 15:13:31 +0100

    removed non-executing code

--- internal/grpc/grpc.go
+++ internal/grpc/grpc.go
@@ -239,19 +239,13 @@ func DialOptions(agentConfig *config.Config, commandConfig *config.Command, reso
 func addTransportCredentials(commandConfig *config.Command, opts []grpc.DialOption) ([]grpc.DialOption, bool) {
 	transportCredentials, err := transportCredentials(commandConfig)
 	if err != nil {
-		if commandConfig.TLS != nil {
-			slog.Error("Unable to add transport credentials to gRPC dial, "+
-				"failed to parse configuration: ", "error", err)
+		slog.Error("Unable to add transport credentials to gRPC dial, "+
+			"failed to parse configuration", "error", err)
 
-			return opts, false
-		}
-
-		slog.Debug("No TLS configured, Adding default transport credentials to gRPC dial options")
-		opts = append(opts, grpc.WithTransportCredentials(defaultCredentials))
-
-		return opts, true
+		return opts, false
 	}
 
+	slog.Debug("Adding transport credentials to gRPC dial options")
 	opts = append(opts, grpc.WithTransportCredentials(transportCredentials))
 
 	return opts, false
